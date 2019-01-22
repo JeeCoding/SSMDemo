1c623d2e1287f5c42864a376f6b3f7464e457c50
RuPorcelain <251682553@qq.com>
2019-01-22 16:29:44 +0800

    数据库sys

---
+++ db/sys/ssm_demo_mysql.sql
@@ -0,0 +1,324 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : mysql
+Source Server Version : 50724
+Source Host           : localhost:3306
+Source Database       : ssm_demo
+
+Target Server Type    : MYSQL
+Target Server Version : 50724
+File Encoding         : 65001
+
+Date: 2019-01-22 16:27:43
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for k_dept
+-- ----------------------------
+DROP TABLE IF EXISTS `k_dept`;
+CREATE TABLE `k_dept` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `name` varchar(255) DEFAULT NULL COMMENT '姓名',
+  `age` int(11) DEFAULT NULL COMMENT '年龄',
+  `birdate` datetime DEFAULT NULL COMMENT '出生日期',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of k_dept
+-- ----------------------------
+INSERT INTO `k_dept` VALUES ('1', '小明', '20', '2018-12-22 00:00:00');
+INSERT INTO `k_dept` VALUES ('2', '小明', '18', '2027-04-23 00:00:00');
+INSERT INTO `k_dept` VALUES ('3', '小明', '18', '2027-04-23 00:00:00');
+INSERT INTO `k_dept` VALUES ('4', '小明', '20', '2018-12-24 00:00:00');
+INSERT INTO `k_dept` VALUES ('5', '小明', '20', '2019-01-16 00:00:00');
+INSERT INTO `k_dept` VALUES ('6', '小明', '20', '2019-01-17 00:00:00');
+INSERT INTO `k_dept` VALUES ('7', '小明', '20', '2019-01-17 00:19:08');
+
+-- ----------------------------
+-- Table structure for sys_area
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_area`;
+CREATE TABLE `sys_area` (
+  `id` varchar(64) NOT NULL COMMENT '编号',
+  `parent_id` varchar(64) NOT NULL COMMENT '父级编号',
+  `parent_ids` varchar(2000) NOT NULL COMMENT '所有父级编号',
+  `name` varchar(100) NOT NULL COMMENT '名称',
+  `sort` decimal(10,0) NOT NULL COMMENT '排序',
+  `code` varchar(100) DEFAULT NULL COMMENT '区域编码',
+  `type` char(1) DEFAULT NULL COMMENT '区域类型',
+  `create_by` varchar(64) NOT NULL COMMENT '创建者',
+  `create_date` datetime NOT NULL COMMENT '创建时间',
+  `update_by` varchar(64) NOT NULL COMMENT '更新者',
+  `update_date` datetime NOT NULL COMMENT '更新时间',
+  `remarks` varchar(255) DEFAULT NULL COMMENT '备注信息',
+  `del_flag` char(1) NOT NULL DEFAULT '0' COMMENT '删除标记',
+  PRIMARY KEY (`id`),
+  KEY `sys_area_parent_id` (`parent_id`),
+  KEY `sys_area_del_flag` (`del_flag`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='区域表';
+
+-- ----------------------------
+-- Records of sys_area
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for sys_dict
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_dict`;
+CREATE TABLE `sys_dict` (
+  `id` varchar(64) NOT NULL COMMENT '编号',
+  `value` varchar(100) NOT NULL COMMENT '数据值',
+  `label` varchar(100) NOT NULL COMMENT '标签名',
+  `type` varchar(100) NOT NULL COMMENT '类型',
+  `description` varchar(100) NOT NULL COMMENT '描述',
+  `sort` decimal(10,0) NOT NULL COMMENT '排序（升序）',
+  `parent_id` varchar(64) DEFAULT '0' COMMENT '父级编号',
+  `create_by` varchar(64) NOT NULL COMMENT '创建者',
+  `create_date` datetime NOT NULL COMMENT '创建时间',
+  `update_by` varchar(64) NOT NULL COMMENT '更新者',
+  `update_date` datetime NOT NULL COMMENT '更新时间',
+  `remarks` varchar(255) DEFAULT NULL COMMENT '备注信息',
+  `del_flag` char(1) NOT NULL DEFAULT '0' COMMENT '删除标记',
+  PRIMARY KEY (`id`),
+  KEY `sys_dict_value` (`value`),
+  KEY `sys_dict_label` (`label`),
+  KEY `sys_dict_del_flag` (`del_flag`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='字典表';
+
+-- ----------------------------
+-- Records of sys_dict
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for sys_log
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_log`;
+CREATE TABLE `sys_log` (
+  `id` varchar(64) NOT NULL COMMENT '编号',
+  `type` char(1) DEFAULT '1' COMMENT '日志类型',
+  `title` varchar(255) DEFAULT '' COMMENT '日志标题',
+  `create_by` varchar(64) DEFAULT NULL COMMENT '创建者',
+  `create_date` datetime DEFAULT NULL COMMENT '创建时间',
+  `remote_addr` varchar(255) DEFAULT NULL COMMENT '操作IP地址',
+  `user_agent` varchar(255) DEFAULT NULL COMMENT '用户代理',
+  `request_uri` varchar(255) DEFAULT NULL COMMENT '请求URI',
+  `method` varchar(5) DEFAULT NULL COMMENT '操作方式',
+  `params` text COMMENT '操作提交的数据',
+  `exception` text COMMENT '异常信息',
+  PRIMARY KEY (`id`),
+  KEY `sys_log_create_by` (`create_by`),
+  KEY `sys_log_request_uri` (`request_uri`),
+  KEY `sys_log_type` (`type`),
+  KEY `sys_log_create_date` (`create_date`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='日志表';
+
+-- ----------------------------
+-- Records of sys_log
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for sys_mdict
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_mdict`;
+CREATE TABLE `sys_mdict` (
+  `id` varchar(64) NOT NULL COMMENT '编号',
+  `parent_id` varchar(64) NOT NULL COMMENT '父级编号',
+  `parent_ids` varchar(2000) NOT NULL COMMENT '所有父级编号',
+  `name` varchar(100) NOT NULL COMMENT '名称',
+  `sort` decimal(10,0) NOT NULL COMMENT '排序',
+  `description` varchar(100) DEFAULT NULL COMMENT '描述',
+  `create_by` varchar(64) NOT NULL COMMENT '创建者',
+  `create_date` datetime NOT NULL COMMENT '创建时间',
+  `update_by` varchar(64) NOT NULL COMMENT '更新者',
+  `update_date` datetime NOT NULL COMMENT '更新时间',
+  `remarks` varchar(255) DEFAULT NULL COMMENT '备注信息',
+  `del_flag` char(1) NOT NULL DEFAULT '0' COMMENT '删除标记',
+  PRIMARY KEY (`id`),
+  KEY `sys_mdict_parent_id` (`parent_id`),
+  KEY `sys_mdict_del_flag` (`del_flag`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='多级字典表';
+
+-- ----------------------------
+-- Records of sys_mdict
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for sys_menu
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_menu`;
+CREATE TABLE `sys_menu` (
+  `id` varchar(64) NOT NULL COMMENT '编号',
+  `parent_id` varchar(64) NOT NULL COMMENT '父级编号',
+  `parent_ids` varchar(2000) NOT NULL COMMENT '所有父级编号',
+  `name` varchar(100) NOT NULL COMMENT '名称',
+  `sort` decimal(10,0) NOT NULL COMMENT '排序',
+  `href` varchar(2000) DEFAULT NULL COMMENT '链接',
+  `target` varchar(20) DEFAULT NULL COMMENT '目标',
+  `icon` varchar(100) DEFAULT NULL COMMENT '图标',
+  `is_show` char(1) NOT NULL COMMENT '是否在菜单中显示',
+  `permission` varchar(200) DEFAULT NULL COMMENT '权限标识',
+  `create_by` varchar(64) NOT NULL COMMENT '创建者',
+  `create_date` datetime NOT NULL COMMENT '创建时间',
+  `update_by` varchar(64) NOT NULL COMMENT '更新者',
+  `update_date` datetime NOT NULL COMMENT '更新时间',
+  `remarks` varchar(255) DEFAULT NULL COMMENT '备注信息',
+  `del_flag` char(1) NOT NULL DEFAULT '0' COMMENT '删除标记',
+  PRIMARY KEY (`id`),
+  KEY `sys_menu_parent_id` (`parent_id`),
+  KEY `sys_menu_del_flag` (`del_flag`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='菜单表';
+
+-- ----------------------------
+-- Records of sys_menu
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for sys_office
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_office`;
+CREATE TABLE `sys_office` (
+  `id` varchar(64) NOT NULL COMMENT '编号',
+  `parent_id` varchar(64) NOT NULL COMMENT '父级编号',
+  `parent_ids` varchar(2000) NOT NULL COMMENT '所有父级编号',
+  `name` varchar(100) NOT NULL COMMENT '名称',
+  `sort` decimal(10,0) NOT NULL COMMENT '排序',
+  `area_id` varchar(64) NOT NULL COMMENT '归属区域',
+  `code` varchar(100) DEFAULT NULL COMMENT '区域编码',
+  `type` char(1) NOT NULL COMMENT '机构类型',
+  `grade` char(1) NOT NULL COMMENT '机构等级',
+  `address` varchar(255) DEFAULT NULL COMMENT '联系地址',
+  `zip_code` varchar(100) DEFAULT NULL COMMENT '邮政编码',
+  `master` varchar(100) DEFAULT NULL COMMENT '负责人',
+  `phone` varchar(200) DEFAULT NULL COMMENT '电话',
+  `fax` varchar(200) DEFAULT NULL COMMENT '传真',
+  `email` varchar(200) DEFAULT NULL COMMENT '邮箱',
+  `USEABLE` varchar(64) DEFAULT NULL COMMENT '是否启用',
+  `PRIMARY_PERSON` varchar(64) DEFAULT NULL COMMENT '主负责人',
+  `DEPUTY_PERSON` varchar(64) DEFAULT NULL COMMENT '副负责人',
+  `create_by` varchar(64) NOT NULL COMMENT '创建者',
+  `create_date` datetime NOT NULL COMMENT '创建时间',
+  `update_by` varchar(64) NOT NULL COMMENT '更新者',
+  `update_date` datetime NOT NULL COMMENT '更新时间',
+  `remarks` varchar(255) DEFAULT NULL COMMENT '备注信息',
+  `del_flag` char(1) NOT NULL DEFAULT '0' COMMENT '删除标记',
+  PRIMARY KEY (`id`),
+  KEY `sys_office_parent_id` (`parent_id`),
+  KEY `sys_office_del_flag` (`del_flag`),
+  KEY `sys_office_type` (`type`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='机构表';
+
+-- ----------------------------
+-- Records of sys_office
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for sys_role
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_role`;
+CREATE TABLE `sys_role` (
+  `id` varchar(64) NOT NULL COMMENT '编号',
+  `office_id` varchar(64) DEFAULT NULL COMMENT '归属机构',
+  `name` varchar(100) NOT NULL COMMENT '角色名称',
+  `enname` varchar(255) DEFAULT NULL COMMENT '英文名称',
+  `role_type` varchar(255) DEFAULT NULL COMMENT '角色类型',
+  `data_scope` char(1) DEFAULT NULL COMMENT '数据范围',
+  `is_sys` varchar(64) DEFAULT NULL COMMENT '是否系统数据',
+  `useable` varchar(64) DEFAULT NULL COMMENT '是否可用',
+  `create_by` varchar(64) NOT NULL COMMENT '创建者',
+  `create_date` datetime NOT NULL COMMENT '创建时间',
+  `update_by` varchar(64) NOT NULL COMMENT '更新者',
+  `update_date` datetime NOT NULL COMMENT '更新时间',
+  `remarks` varchar(255) DEFAULT NULL COMMENT '备注信息',
+  `del_flag` char(1) NOT NULL DEFAULT '0' COMMENT '删除标记',
+  PRIMARY KEY (`id`),
+  KEY `sys_role_del_flag` (`del_flag`),
+  KEY `sys_role_enname` (`enname`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='角色表';
+
+-- ----------------------------
+-- Records of sys_role
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for sys_role_menu
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_role_menu`;
+CREATE TABLE `sys_role_menu` (
+  `role_id` varchar(64) NOT NULL COMMENT '角色编号',
+  `menu_id` varchar(64) NOT NULL COMMENT '菜单编号',
+  PRIMARY KEY (`role_id`,`menu_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='角色-菜单';
+
+-- ----------------------------
+-- Records of sys_role_menu
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for sys_role_office
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_role_office`;
+CREATE TABLE `sys_role_office` (
+  `role_id` varchar(64) NOT NULL COMMENT '角色编号',
+  `office_id` varchar(64) NOT NULL COMMENT '机构编号',
+  PRIMARY KEY (`role_id`,`office_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='角色-机构';
+
+-- ----------------------------
+-- Records of sys_role_office
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for sys_user
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_user`;
+CREATE TABLE `sys_user` (
+  `id` varchar(64) NOT NULL COMMENT '编号',
+  `company_id` varchar(64) NOT NULL COMMENT '归属公司',
+  `office_id` varchar(64) NOT NULL COMMENT '归属部门',
+  `login_name` varchar(100) NOT NULL COMMENT '登录名',
+  `password` varchar(100) NOT NULL COMMENT '密码',
+  `no` varchar(100) DEFAULT NULL COMMENT '工号',
+  `name` varchar(100) NOT NULL COMMENT '姓名',
+  `email` varchar(200) DEFAULT NULL COMMENT '邮箱',
+  `phone` varchar(200) DEFAULT NULL COMMENT '电话',
+  `mobile` varchar(200) DEFAULT NULL COMMENT '手机',
+  `user_type` char(1) DEFAULT NULL COMMENT '用户类型',
+  `photo` varchar(1000) DEFAULT NULL COMMENT '用户头像',
+  `login_ip` varchar(100) DEFAULT NULL COMMENT '最后登陆IP',
+  `login_date` datetime DEFAULT NULL COMMENT '最后登陆时间',
+  `login_flag` varchar(64) DEFAULT NULL COMMENT '是否可登录',
+  `create_by` varchar(64) NOT NULL COMMENT '创建者',
+  `create_date` datetime NOT NULL COMMENT '创建时间',
+  `update_by` varchar(64) NOT NULL COMMENT '更新者',
+  `update_date` datetime NOT NULL COMMENT '更新时间',
+  `remarks` varchar(255) DEFAULT NULL COMMENT '备注信息',
+  `del_flag` char(1) NOT NULL DEFAULT '0' COMMENT '删除标记',
+  PRIMARY KEY (`id`),
+  KEY `sys_user_office_id` (`office_id`),
+  KEY `sys_user_login_name` (`login_name`),
+  KEY `sys_user_company_id` (`company_id`),
+  KEY `sys_user_update_date` (`update_date`),
+  KEY `sys_user_del_flag` (`del_flag`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户表';
+
+-- ----------------------------
+-- Records of sys_user
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for sys_user_role
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_user_role`;
+CREATE TABLE `sys_user_role` (
+  `user_id` varchar(64) NOT NULL COMMENT '用户编号',
+  `role_id` varchar(64) NOT NULL COMMENT '角色编号',
+  PRIMARY KEY (`user_id`,`role_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户-角色';
+
+-- ----------------------------
+-- Records of sys_user_role
+-- ----------------------------
