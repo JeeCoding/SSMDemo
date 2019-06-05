774f988cab89463049d8a87dbf2b74b1b4acf266
RuPorcelain <251682553@qq.com>
2019-06-05 17:44:09 +0800

    整合shiro

---
+++ src/main/java/com/huzhframework/ssmdemo/sys/shiro/UserRealm.java
@@ -0,0 +1,67 @@
+package com.huzhframework.ssmdemo.sys.shiro;
+
+import com.huzhframework.ssmdemo.dao.UserMapper;
+import com.huzhframework.ssmdemo.entity.User;
+import com.sun.mail.iap.ByteArray;
+import org.apache.shiro.authc.*;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.util.ByteSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @ClassName UserRealm
+ * @Description TODO
+ * @Date 2019/6/5 17:03
+ * @Author huzh
+ * @Version 1.0
+ */
+@Service
+public class UserRealm extends AuthorizingRealm {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
+        String username = (String) principals.getPrimaryPrincipal();
+
+        SimpleAuthorizationInfo authorizationInfo = new SimpleAuthorizationInfo();
+        Set<String> roles = new HashSet<String>();
+        Set<String> Permissions = new HashSet<String>();
+        authorizationInfo.setRoles(roles);
+        authorizationInfo.setStringPermissions(Permissions);
+        return authorizationInfo;
+    }
+
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+        String username = (String) token.getPrincipal();
+        User user = userMapper.selectByUsername(username);
+
+        if (user == null) {
+            //没找到帐号
+            throw new UnknownAccountException();
+        }
+
+        if (Boolean.TRUE.equals(user.getLocked())) {
+            //帐号锁定
+            throw new LockedAccountException();
+        }
+
+        //交给AuthenticatingRealm使用CredentialsMatcher进行密码匹配，如果觉得人家的不好可以自定义实现
+        SimpleAuthenticationInfo authenticationInfo = new SimpleAuthenticationInfo(
+                user.getUsername(),
+                user.getPassword(),
+                ByteSource.Util.bytes(user.getCredentialsSalt()),
+                getName()
+        );
+        return authenticationInfo;
+    }
+}
